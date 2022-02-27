375793da8bba77c2125071a01035b659db7d08ea
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-02-27 11:08:20 -0600

    commit tree c6281a9c5f2a3aebf6707b7bac8877b401ca50e6

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (array) {
+ const array2 = array.slice(0, 2,);
+ return array2;
+ }
+ const returnLastTwoDrivers = function (array) {
+     const array2 = array.slice(2);
+     return array2;
+ }
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+ 
+ function createFareMultiplier(integer) {
+    const expensive = function (fare) {return fare * integer};
+    return expensive;
+ }
+
+ //const fareDoubler = createFareMultiplier() {}
+ 
+const fareDoubler = createFareMultiplier(2); 
+const fareTripler = createFareMultiplier(3);
+/*function (doubler) {
+     const doubler2 =  doubler * 2;
+     return doubler2;
+ }
+ 
+ const fareTripler = function (tripler) {
+     const Tripler2 = tripler * 3;
+     return Tripler2;
+ } */
+function selectDifferentDrivers(arrayofDrivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(arrayofDrivers);
+}
+ 
